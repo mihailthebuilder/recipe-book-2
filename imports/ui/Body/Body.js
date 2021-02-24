a55c5f076a68f0634e1510d276e0e3740e2f3e39
Mihail Marian <mihailmarian12@gmail.com>
2021-02-24 14:28:08 +0000

    got flow router working

--- imports/ui/Body/Body.js
+++ imports/ui/Body/Body.js
@@ -1,1 +1,17 @@
 import "./Body.html";
+import "../Home/Home.html";
+import "../Test/Test.html";
+
+FlowRouter.route("/", {
+  name: "home",
+  action: function () {
+    BlazeLayout.render("Home");
+  },
+});
+
+FlowRouter.route("/test", {
+  name: "test",
+  action: function () {
+    BlazeLayout.render("Test");
+  },
+});
